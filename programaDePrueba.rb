adf2a312136a4ac01182268fa047e41012f340ea
Jose A. Arroyo <j.a.arroyo9@gmail.com>
2016-09-22 10:19:27 +0200

    Subir primer programa ruby

---
+++ programaDePrueba.rb
@@ -0,0 +1,31 @@
+class Anidados                                                                           
+
+      def initialize()
+      end
+      def saluda() 
+            puts "Ingrese un numero:"
+            STDOUT.flush
+            i=gets.chomp
+            i=i.to_i
+            if i>0
+                puts "El numero es positivo"
+            if(i%2==0)
+         puts "El numero es par"
+     else
+         puts "El numero es impar"
+     end
+            elsif i<0
+                puts "El numero es negativo"
+            if(i%2==0)
+         puts "El numero es par"
+     else
+         puts "El numero es impar"
+            end
+            else
+                print "El numero es 0"
+            end
+        end
+    end
+    objeto = Anidados.new() 
+    objeto.saluda
+    gets() 
